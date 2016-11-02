efc4dd20b7ed5acab4817a1cc1985088a13b8fb6
foxmo <foxmo44@yahoo.com>
2016-11-01 21:15:24 -0500

    place holder main function

---
+++ src/Project06Fox.java
@@ -0,0 +1,17 @@
+/**
+ * <h1>Advanced Java - Project06Fox</h1>
+ * <h1>Project06Fox  Class</h1>
+ * This is the class description XXXXXXX
+ * <p>
+ * <b>Create Date: 11/1/2016</b>
+ * <b>Due Date: XXXXXXXX</b>
+ *
+ * @author Michael Fox
+ */
+public class Project06Fox
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Place Holder");
+    }
+}
